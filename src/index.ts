81895fe46328a29bdce46104df686efb86a4c123
Seti Ngabo <seti.ngabo@mths.ca>
2022-11-16 16:59:09 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+/**
+ * Factorial program
+ *
+ * By:      Seti Ngabo
+ * Version: 1.0
+ * Since:   2022-11-07
+ */
+
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+
+/**
+ * factorial
+ * 
+ * @param {number} accepts a string
+ * @returns {number} returns, using recursion
+ */
+function factorial(num: number): number {
+  let firstInt
+  if (num === 0) {
+    firstInt = 1
+  } else if (num < 0) {
+    firstInt = -1
+  } else {
+    firstInt = factorial(num - 1) * num
+  }
+  return firstInt
+}
+
+// input
+const userInput = prompt('Enter a number: ')
+try {
+  const userNum = parseInt(userInput)
+  if (isNaN(userNum)) {
+    console.log('Invalid Input, try again.')
+  } else {
+    console.log(`${userNum}! = ${factorial(userNum)}`)
+  }
+} catch (e) {
+  console.log('ERROR:')
+}
+
+console.log('\nDone.')
